5ee22707262ce12dea74b89cf11c8bbe70b425aa
Топчу Владислав <v.topchu@affstar.com>
2022-01-10 00:07:46 +0300

    refactor(general): improved generate script logic

--- generateBoilerplate.js
+++ generateBoilerplate.js
@@ -7,6 +7,13 @@ Object.defineProperty(String.prototype, 'capitalize', {
 	enumerable: false
 });
 
+Object.defineProperty(String.prototype, 'decapitalize', {
+	value: function () {
+		return this.charAt(0).toLowerCase() + this.slice(1);
+	},
+	enumerable: false
+});
+
 Object.defineProperty(String.prototype, 'removeLastChar', {
 	value: function () {
 		return this.slice(0, this.length - 1) + this.slice(this.length, this.length)
@@ -14,60 +21,32 @@ Object.defineProperty(String.prototype, 'removeLastChar', {
 	enumerable: false
 })
 
-const _name = process.argv[3]
+let _folderName = undefined
+let _name = process.argv[3]
+_folderName = _name
+let _splittedName = _name.split('-')
+if (_splittedName.length > 1) {
+	_name = _splittedName.map(el => el.capitalize()).join('')
+}
+console.log("NAME: ");
+console.log(_name);
+console.log("FOLDERNAME: ");
+console.log(_folderName);
+
 
 if (['g', 'generate', 'gen'].includes(process.argv[2])) {
 
-	if (!fs.existsSync(`./src/${_name}`)) {
-		fs.mkdirSync(`./src/${_name}`)
+	if (!fs.existsSync(`./src/${_folderName}`)) {
+		fs.mkdirSync(`./src/${_folderName}`)
+		fs.mkdirSync(`./src/${_folderName}/dto`)
 	}
 
 	if (!process.argv.includes('--no-schema')) {
-		createSchema(_name)
-	}
-	createController(_name)
-	createService(_name)
-	createModule(_name)
-
-	function createController(name) {
-		let fileName = `${name}.controller.ts`
-		let officialName = name.capitalize()
-		let className = officialName + "Controller"
-		fs.writeFileSync(`./src/${_name}/${fileName}`, `import { Controller, Get } from '@nestjs/common';
-import { ${officialName}Service } from './${name}.service';
-
-@Controller()
-export class ${className} {
-	constructor(private readonly ${name}Service: ${officialName}Service) { }
-
-	@Get()
-	getSomething(): any {
-		return ;
-	}
-}`)
-	}
-
-	function createService(name) {
-		let fileName = `${name}.service.ts`
-		let officialName = name.capitalize()
-		fs.writeFileSync(`./src/${_name}/${fileName}`, `import { Injectable } from "@nestjs/common";
-import { ${officialName.removeLastChar()} } from "./${name.removeLastChar()}.schema";
-
-@Injectable()
-export class ${officialName}Service {
-	constructor(private readonly repository: Repository) { }
-
-	async getSomething(): Promise<any> {
-		return ;
-	}
-}`)
-	}
-
-	function createSchema(name) {
-		let currentName = name.removeLastChar()
+		// Create Schema
+		let currentName = _folderName.removeLastChar()
 		let fileName = `${currentName}.schema.ts`
-		let officialName = currentName.capitalize()
-		fs.writeFileSync(`./src/${_name}/${fileName}`, `import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+		let officialName = _name.removeLastChar()
+		fs.writeFileSync(`./src/${_folderName}/${fileName}`, `import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
 export type ${officialName}Document = ${officialName} & Document;
@@ -81,38 +60,66 @@ export class ${officialName} {
 export const ${officialName}Schema = SchemaFactory.createForClass(${officialName});`)
 	}
 
-	function createModule(name) {
-		let fileName = `${name}.module.ts`
-		let officialName = name.capitalize()
-		const generateContent = () => {
-			if (process.argv.includes('--no-schema')) {
-				return `import { Module } from "@nestjs/common";
-import { ${officialName}Controller } from "./${name}.controller";
-import { ${officialName}Service } from "./${name}.service";
+	// Create Controller
+	let fileName = `${_folderName}.controller.ts`
+	let className = _name + "Controller"
+	fs.writeFileSync(`./src/${_folderName}/${fileName}`, `import { Controller, Get } from '@nestjs/common';
+import { ${_name}Service } from './${_folderName}.service';
+
+@Controller()
+export class ${className} {
+	constructor(private readonly ${_name.decapitalize()}Service: ${_name}Service) { }
+
+	@Get()
+	getSomething(): any {
+		return ;
+	}
+}`)
+
+	// Create Service
+	fileName = `${_folderName}.service.ts`
+	fs.writeFileSync(`./src/${_folderName}/${fileName}`, `import { Injectable } from "@nestjs/common";
+import { ${_name.removeLastChar()} } from "./${_folderName.removeLastChar()}.schema";
+
+@Injectable()
+export class ${_name}Service {
+constructor(private readonly repository: Repository) { }
+
+async getSomething(): Promise<any> {
+	return ;
+}
+}`)
+
+	// Create Module
+	fileName = `${_folderName}.module.ts`
+	const generateContent = () => {
+		if (process.argv.includes('--no-schema')) {
+			return `import { Module } from "@nestjs/common";
+import { ${_name}Controller } from "./${_folderName}.controller";
+import { ${_name}Service } from "./${_folderName}.service";
 
 @Module({
 	imports: [],
-	controllers: [${officialName}Controller],
-	providers: [${officialName}Service]
+	controllers: [${_name}Controller],
+	providers: [${_name}Service]
 })
 
-export class ${officialName}Module { }`
-			} else {
-				return `import { Module } from "@nestjs/common";
+export class ${_name}Module { }`
+		} else {
+			return `import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ${officialName}Controller } from "./${name}.controller";
-import { ${officialName.removeLastChar()}, ${officialName.removeLastChar()}Schema } from "./${name.removeLastChar()}.schema";
-import { ${officialName}Service } from "./${name}.service";
+import { ${_name}Controller } from "./${_folderName}.controller";
+import { ${_name.removeLastChar()}, ${_name.removeLastChar()}Schema } from "./${_folderName.removeLastChar()}.schema";
+import { ${_name}Service } from "./${_folderName}.service";
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: ${officialName.removeLastChar()}.name, schema: ${officialName.removeLastChar()}Schema }])],
-	controllers: [${officialName}Controller],
-	providers: [${officialName}Service]
+	imports: [MongooseModule.forFeature([{ name: ${_name.removeLastChar()}.name, schema: ${_name.removeLastChar()}Schema }])],
+	controllers: [${_name}Controller],
+	providers: [${_name}Service]
 })
 
-export class ${officialName}Module { }`
-			}
+export class ${_name}Module { }`
 		}
-		fs.writeFileSync(`./src/${_name}/${fileName}`, generateContent())
 	}
+	fs.writeFileSync(`./src/${_folderName}/${fileName}`, generateContent())
 }
